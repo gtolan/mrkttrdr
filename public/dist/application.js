904e66396037d0e01706d9b214035394b5983f44
gtolan <tolangerard@gmail.com>
2016-10-07 14:37:03 +0100

    Fri1434

--- public/dist/application.js
+++ public/dist/application.js
@@ -915,34 +915,17 @@ angular.module('core').controller('ModalLoginCtrl', ['$scope', '$state',
         $log.info('Modal dismissed at: ' + new Date());
       });
     };
-
-    var openLogin = function(size) {
-      var modalInstance = $modal.open({
-        templateUrl: 'myModalContent.html',
-        controller: 'ModalInstanceCtrl',
-        size: size,
-        resolve: {
-          items: function() {
-            return $scope.items;
-          }
-        }
-      });
-      modalInstance.result.then(function(selectedItem) {
-        $scope.selected = selectedItem;
-      }, function() {
-        $log.info('Modal dismissed at: ' + new Date());
-      });
-    };
   }
 ]);
 // Please note that $modalInstance represents a modal window (instance) dependency.
 // It is not the same as the $modal service used above.
-angular.module('core').controller('ModalInstanceCtrl', ["$scope", "$modalInstance", "items", function($scope,
+angular.module('core').controller('ModalInstanceCtrl', ["$scope", "Authentication", "$modalInstance", "items", function($scope, Authentication,
   $modalInstance, items) {
   $scope.items = items;
   $scope.selected = {
     item: $scope.items[0]
   };
+  $scope.authentication = Authentication;
   $scope.ok = function() {
     $modalInstance.close($scope.selected.item);
   };
